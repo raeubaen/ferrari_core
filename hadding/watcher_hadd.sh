c372c79dc3ff3e7ba7c6023c532fa60723c6eb7b
ecalgit <ecalgit@cern.ch>
2026-06-09 10:49:15 +0200

    bugfix

--- hadding/watcher_hadd.sh
+++ hadding/watcher_hadd.sh
@@ -4,7 +4,7 @@ HADD_GLOB_BUFFER="$PLOT_MAIN_FOLDER/to_hadd_buffer.txt"
 while true; do
   if [ -e ${HADD_NOW_DIRS} ]; then
     echo $HADD_NOW_DIRS exists, starting
-    timeout 40s bash hadd.sh > $PLOT_MAIN_FOLDER/logs/hadd_$(date | sed 's/ /-/g') 2>&1;
+    timeout 40s bash hadd.sh 2>&1 | tee $PLOT_MAIN_FOLDER/logs/hadd_$(date | sed 's/ /-/g');
   fi
   echo $HADD_NOW_DIRS not found, sleeping 2 seconds and retrying
   echo "for DEBUG, cat of ${HADD_GLOB_BUFFER}"
