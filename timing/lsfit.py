3ab2d29a8a0cfc592ac643cc3165f7a385dca1e9
root <root@crilin.cern.ch>
2026-07-13 10:00:07 +0200

    bugfix

--- timing/lsfit.py
+++ timing/lsfit.py
@@ -171,7 +171,6 @@ def fit_pulse_iterative(waveforms, pulse, t, t_data_peak, t_template_peak, n_ite
     P_fit, _ = pulse.eval_and_derivative(t_shift)          # (EC, N)
     model = A[:, None] * P_fit
 
-    '''
     with open("fit_debug.csv", "a", newline="") as f:
         writer = csv.writer(f)
 
@@ -186,16 +185,7 @@ def fit_pulse_iterative(waveforms, pulse, t, t_data_peak, t_template_peak, n_ite
                     model[ch,i]      # fitted template
                 ])
     '''
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     return A, dt
 
 
